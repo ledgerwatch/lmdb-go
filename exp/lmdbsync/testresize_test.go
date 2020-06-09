e39320030e660923810bd1690b1fbd6e5cdd9ab6
alex.sharov <AskAlexSharov@gmail.com>
2020-06-09 13:06:29 +0700

    add github ci

--- exp/lmdbsync/testresize_test.go
+++ exp/lmdbsync/testresize_test.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"runtime"
 	"testing"
 	"time"
 
@@ -62,11 +61,7 @@ func TestResize(t *testing.T) {
 	}
 
 	bin := filepath.Join(tempdir, "testresize")
-	ss := "./testresize"
-	if runtime.GOOS == "windows" {
-		ss = ".\\testresize"
-	}
-	build := exec.Command("go", "build", "-o", bin, ss)
+	build := exec.Command("go", "build", "-o", bin, "'github.com/AskAlexSharov/lmdb-go/testresize'")
 	//build.Dir = tempdir
 	build.Stderr = os.Stderr
 	err = build.Run()
