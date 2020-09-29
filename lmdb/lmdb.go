ebe845531ee1419fbff08d435b8f7c095928761a
alex.sharov <AskAlexSharov@gmail.com>
2020-09-29 14:56:18 +0700

    lmdb debug=2

--- lmdb/lmdb.go
+++ lmdb/lmdb.go
@@ -132,7 +132,8 @@ package lmdb
 
 /*
 #cgo CFLAGS: -pthread -W -Wall -Wno-implicit-fallthrough -Wno-unused-parameter -Wno-format-extra-args -Wbad-function-cast -Wno-missing-field-initializers -O2 -g
-#cgo linux,pwritev CFLAGS: -DMDB_USE_PWRITEV -DMDB_DEBUG=1
+#cgo linux,pwritev CFLAGS: -DMDB_USE_PWRITEV
+#cgo CFLAGS: -DMDB_DEBUG=1
 
 #include "lmdb.h"
 */
